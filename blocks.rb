02d3c239380bf9ab175a89740e68dee51f5649d7
Ruslan Kenzhebekov <reke.essential@gmail.com>
2013-11-24 08:28:11 +0600

    scope gates

--- blocks.rb
+++ blocks.rb
@@ -33,6 +33,24 @@ MyClass = Class.new do
   end
 end
 
+def shared_methods
+  shared = 0
+
+  Kernel.send :define_method, :counter do
+    shared
+  end
+
+  Kernel.send :define_method, :inc do |x|
+    shared += x
+  end
+end
+
+
+p shared_methods
+p counter
+p inc(1)
+
+
 m = MyClass.new
 m.my_method
 
